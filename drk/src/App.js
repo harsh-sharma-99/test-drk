b2e872e48bf53645940175c414b0450c968607de
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2019-10-07 18:28:17 +0530

    Initial Commit

---
+++ drk/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { Button, Spinner } from 'design-react-kit';
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import 'typeface-titillium-web';
+import 'typeface-roboto-mono';
+import 'typeface-lora';
+
+import NewComponent from './NewComponent'
+
+function App() {
+  return (
+    <div className="App">
+    <div className="App-header">
+      <NewComponent/>
+      <Button color="primary">Hello</Button>
+      <button>Hello</button>
+      <p className="p-3">
+      <Spinner active/>
+
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
